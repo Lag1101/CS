14c884878ef2f6997983538d47b8e8db3e409476
lag1101 <lomanovvasiliy@gmail.com>
2013-09-11 00:28:17 +0400

    initial commit basics of engine and visualization

---
+++ engine.ts
@@ -0,0 +1,50 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: loman_000
+ * Date: 10.09.13
+ * Time: 22:11
+ * To change this template use File | Settings | File Templates.
+ */
+
+export var FieldType:Array = [
+    "forest",
+    "city"
+];
+
+
+export var UnitType = [
+    "sniper",
+    "engineer",
+    "soldier"
+];
+
+export class Unit{
+    type:string;
+    constructor(type:string) {
+        this.type = type;
+    }
+}
+export class Ceil{
+    type:string;
+    units:Array;
+    constructor(type:string, units:Array) {
+        this.type = type;
+        this.units = units;
+    }
+}
+
+export class Field{
+    map:Array;
+    width:number;
+    height:number;
+    constructor(width:number, height:number) {
+        this.width = width;
+        this.height = height;
+        var line:Array = [];
+        for( var i = 0; i < width; i++ ) line.push( new Ceil( FieldType[0], []) );
+        for( var i = 0; i < height; i++ ) this.map.push(line);
+    }
+    get(x:number, y:number):Ceil{
+        return this.map[y][x];
+    }
+}
---
+++ visualization.ts
@@ -0,0 +1,20 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: loman_000
+ * Date: 10.09.13
+ * Time: 22:38
+ * To change this template use File | Settings | File Templates.
+ */
+
+import engine = module('engine');
+
+function ShowField(canvas_control:CanvasRenderingContext2D, field:engine.Field):void
+{
+      for(var y = 0; y < field.height; y++)
+      {
+          for( var x = 0; x < field.width; x++ )
+          {
+
+          }
+      }
+}
