bd100346d3f0430b456852d2f2e538e21d9b5059
vasiliy.lomanov <lomanovvasiliy@gmail.com>
2013-09-11 11:47:36 +0400

    fixed bug with exporting ts files need to use <reference path="...ts"/>

--- engine.ts
+++ engine.ts
@@ -5,46 +5,47 @@
  * Time: 22:11
  * To change this template use File | Settings | File Templates.
  */
+module engine{
+    export var FieldType:Array = [
+        "forest",
+        "city"
+    ];
 
-export var FieldType:Array = [
-    "forest",
-    "city"
-];
 
+    export var UnitType = [
+        "sniper",
+        "engineer",
+        "soldier"
+    ];
 
-export var UnitType = [
-    "sniper",
-    "engineer",
-    "soldier"
-];
-
-export class Unit{
-    type:string;
-    constructor(type:string) {
-        this.type = type;
+    export class Unit{
+        type:string;
+        constructor(type:string) {
+            this.type = type;
+        }
     }
-}
-export class Ceil{
-    type:string;
-    units:Array;
-    constructor(type:string, units:Array) {
-        this.type = type;
-        this.units = units;
+    export class Ceil{
+        type:string;
+        units:Array;
+        constructor(type:string, units:Array) {
+            this.type = type;
+            this.units = units;
+        }
     }
-}
 
-export class Field{
-    map:Array;
-    width:number;
-    height:number;
-    constructor(width:number, height:number) {
-        this.width = width;
-        this.height = height;
-        var line:Array = [];
-        for( var i = 0; i < width; i++ ) line.push( new Ceil( FieldType[0], []) );
-        for( var i = 0; i < height; i++ ) this.map.push(line);
+    export class Field{
+        map:Array;
+        width:number;
+        height:number;
+        constructor(width:number, height:number) {
+            this.width = width;
+            this.height = height;
+            var line:Array = [];
+            for( var i = 0; i < width; i++ ) line.push( new Ceil( FieldType[0], []) );
+            for( var i = 0; i < height; i++ ) this.map.push(line);
+        }
+        get(x:number, y:number):Ceil{
+            return this.map[y][x];
+        }
     }
-    get(x:number, y:number):Ceil{
-        return this.map[y][x];
-    }
-}
+};
--- visualization.ts
+++ visualization.ts
@@ -6,15 +6,11 @@
  * To change this template use File | Settings | File Templates.
  */
 
-import engine = module('engine');
+/// <reference path="engine.ts"/>
 
-function ShowField(canvas_control:CanvasRenderingContext2D, field:engine.Field):void
-{
-      for(var y = 0; y < field.height; y++)
-      {
-          for( var x = 0; x < field.width; x++ )
-          {
-
-          }
-      }
-}
+module visualization{
+    function ShowField(canvas_control:CanvasRenderingContext2D, field:engine.Field, ceil_size:number):void {
+        for(var y = 0; y < field.height; y++) for( var x = 0; x < field.width; x++ )
+            canvas_control.fillRect( x*ceil_size, y*ceil_size, (x+1)*ceil_size, (y+1)*ceil_size);
+    }
+};
